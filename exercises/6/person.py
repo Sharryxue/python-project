e13aefb039e1d78fa5dea4a528ef6b0dbdb8571a
Sharry Shi <sharry.shi@outlook.com>
2024-04-14 21:18:37 +0800

    code for 6-4

--- exercises/6/person.py
+++ exercises/6/person.py
@@ -60,3 +60,34 @@ for name, meaning in code_name.items():
 """换行缩进显示."""
 for name,meaning in code_name.items():
     print(name + "\n" + "   " + meaning)
+
+
+# 6-4 词汇表2: 既然你知道了如何遍历字典,现在请整理你为完成练习6-3而编写的代码,将其中的一系列print语句替换为一个遍历字典中的键和值的循环.确定该循环正确无误后,再在词汇表中添加5个Python术语.当你再次运行这个程序的时候,这些新术语将自动包含在输出中.
+
+code_name2 = {
+    "列表":"列表由一系列按特定顺序排列的元素组成.列表放在方括号中.",
+    "元组":"不可变的列表被称为元组,元组看起来像列表,但是由圆括号来表示.",
+    "字典":"字典是一系列键-值对.字典放在花括号中.",
+    "函数":"函数是带名字的代码块,用于完成具体的工作.",
+    "类":"表示现实世界的事务或情形,编写类时,定义一大类都有的通用行为.",
+}
+
+"""同行加冒号':'显示."""
+for name, meaning in code_name2.items():
+    print(name + ":" + meaning)
+
+"""换行缩进显示."""
+for name,meaning in code_name2.items():
+    print(name + "\n" + "   " + meaning)
+
+"""字典中插入数据"""
+code_name2["循环"]="包括for循环和while循环."
+code_name2["条件判断"]="包括if语句, elif, else语句等."
+code_name2["变量"]="变量只能包含字母数字和下划线.变量名可以字母或下划线打头,但不能以数字打头."
+code_name2["数据类型"]="字符串,数字,整数,浮点数."
+code_name2["注释"]="阐述代码要做什么以及如何做."
+
+for name, meaning in code_name2.items():
+    print("\n" + name + ":" + meaning)
+
+
